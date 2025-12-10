2c788d7a00d1aea93cff8fb53aa39408c3719257
Raonson <ehsonmahmadmurodov@gmail.com>
2025-12-10 06:23:52 +0500

    Create app.routes.dart

---
+++ lib/routes/app.routes.dart
@@ -0,0 +1,10 @@
+class AppRoutes {
+  static const String splash = '/';
+  static const String home = '/home';
+  static const String chatGPT = '/chatGPT';
+  static const String kingAI = '/kingAI';
+  static const String reels = '/reels';
+  static const String profile = '/profile';
+  static const String search = '/search';
+  static const String settings = '/settings';
+}
