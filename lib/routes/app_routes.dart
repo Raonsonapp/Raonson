b1d177fcac19374d3c34318012893f2d7546c497
Raonson <ehsonmahmadmurodov@gmail.com>
2025-12-13 21:41:48 +0500

    Update app_routes.dart

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,6 +1,7 @@
 class AppRoutes {
-  static const String splash = '/splash';
-  static const String home = '/home';
-  static const String upload = '/upload';
-  static const String login = '/login';
+  static const chatGPT = '/chatgpt';
+  static const kingAI = '/kingai';
+  static const reels = '/reels';
+  static const profile = '/profile';
+  static const search = '/search';
 }
